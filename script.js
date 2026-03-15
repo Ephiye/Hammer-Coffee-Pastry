fa70031cdf8d0d863a93d7ab052e143b3d9aeaef
Ephiye <epheson2324@gmail.com>
2026-03-15 19:38:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ const menuData = {
         { name: "Chocolate cake", price: 60 },
         { name: "Banana cake", price: 60 },
         { name: "English cake", price: 60 },
-        { name: "Croissant", price: 6 },
-        { name: "Croissa", price: 67 }
+        { name: "Croissant", price: 60 }
     ],
     drinks: {
         hotTea: [
