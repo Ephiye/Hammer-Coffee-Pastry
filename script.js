fa3fad51bf31caf4a7e1eab622452f984cb573c2
Ephiye <epheson2324@gmail.com>
2026-04-13 16:37:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const menuData = {
         { name: "Custard cake", price: 70 },
         { name: "Chocolate cake", price: 70 },
         { name: "Banana cake", price: 60 },
-        { name: "English cake", price: 60 } 
- ],
+        { name: "English cake", price: 60 }                                    
+   ],
    tortas: [
-      {name: "Mille feuille", price: 3,500 },
-      {name: "Custurd cake", price: 3,000 },
-      {name: "Chocolate cake", price: 3,000 },
+      {name: "1kg Mille feuille", price: 3,500 },
+      {name: "1kg Custurd cake", price: 3,000 },
+      {name: "1kg Chocolate cake", price: 3,000 },
     ],
     drinks: {
         hotTea: [
